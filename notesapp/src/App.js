d97a4974da7f1b34ca4ce8cbfadb454cd834c709
SHADAB ANSARI <shadabansari14321@gmail.com>
2023-09-01 14:52:15 +0530

    Storing data permannently

--- notesapp/src/App.js
+++ notesapp/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 import Header from './header';
 import Footer from './footer';
@@ -10,18 +10,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   const [addItem, setAddItem] = useState([]);
 
+
+  // for temperary storing
+  // const addNote = (note) => {
+  //   // alert("i m clicked");
+  //   setAddItem((prevData) => {
+  //     return [...prevData,{...note}]
+  //   });
+  // };
+
+  // for permanent add in localstorage
   const addNote = (note) => {
-    // alert("i m clicked");
-    setAddItem((prevData) => {
-      return [...prevData,{...note}]
-    });
+    // Create a copy of the previous data
+    const newData = [...addItem, { ...note }];
+  
+    // Save the updated data to localStorage
+    localStorage.setItem("notes", JSON.stringify(newData));
+  
+    // Update the state
+    setAddItem(newData);
   };
 
-  const deleteItem = (id) => {
-    setAddItem((prevData) => {
-      return prevData.filter((item, index) => index !== id);
-    });
-  };
+  // for temporary deletion
+  // const deleteItem = (id) => {
+  //   setAddItem((prevData) => {
+  //     return prevData.filter((item, index) => index !== id);
+  //   });
+  // };
+
+
+  // for permanent deletion
+  
+const deleteItem = (id) => {
+  // Create a copy of the previous data and remove the specified note
+  const newData = addItem.filter((item, index) => index !== id);
+
+  // Save the updated data to localStorage
+  localStorage.setItem("notes", JSON.stringify(newData));
+
+  // Update the state
+  setAddItem(newData);
+};
+
+useEffect(() => {
+  // Retrieve the notes from localStorage and parse them
+  const storedNotes = JSON.parse(localStorage.getItem("notes"));
+
+  if (storedNotes) {
+    setAddItem(storedNotes);
+  }
+}, []);
 
   return (
     <div className="App">
