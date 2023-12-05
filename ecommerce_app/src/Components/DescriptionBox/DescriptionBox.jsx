3558f24dc6c997d5c5cccb890a90c280600ee446
SHADAB ANSARI <shadabansari14321@gmail.com>
2023-12-05 12:36:42 +0530

    E-commerce Website

---
+++ ecommerce_app/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Welcome to Shadab Shopping—your one-stop shop for all things Clothes
+          for men,women and kid! Explore our curated selection of top-quality
+          items, enjoy a seamless shopping experience, and benefit from fast,
+          secure delivery. Quality and convenience await you at Shadab Store.
+          Shop now!
+        </p>
+        <p>
+        Welcome to Shadab Shopping—your one-stop shop for all things Clothes
+          for men,women and kid! Explore our curated selection of top-quality
+          items, enjoy a seamless shopping experience, and benefit from fast,
+          secure delivery. Quality and convenience await you at Shadab Store.
+          Shop now!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
